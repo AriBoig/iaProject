eaa80e88e956047e56acdecbd6174ddf33ba1749
Costes <adrien.costes@ecole.ensicaen.fr>
2020-02-06 15:25:25 +0100

    update Coordonnee CentraliserRobot

--- src/map/Gameboard.java
+++ src/map/Gameboard.java
@@ -1,4 +1,4 @@
-package src.map;
+package map;
 
 public class Gameboard {
 
--- src/robot/classes/CentraliserRobot.java
+++ src/robot/classes/CentraliserRobot.java
@@ -1,5 +1,16 @@
 package robot.classes;
 
+import map.Coordonnee;
+import map.TypeCase;
+
+import java.util.ArrayList;
+
+/**
+ * @author Enzo Dechaene
+ * @author Adrien Costes
+ * @date 06/02/2020
+ */
+
 public class CentraliserRobot extends Robot
 {
     private double mineral;
@@ -7,9 +18,32 @@ public class CentraliserRobot extends Robot
     private double water;
     private double consumeWater;
 
+    private ArrayList<Coordonnee> mineralPosition;
+
 
     public CentraliserRobot()
     {
         super();
+    }
+
+
+    public void collectInfo(TypeCase typeCase, Coordonnee coordonnee) {
+        //TODO
+    }
+
+    public double getMineral() {
+        return mineral;
+    }
+
+    public double getFood() {
+        return food;
+    }
+
+    public double getWater() {
+        return water;
+    }
+
+    public double getConsumeWater() {
+        return consumeWater;
     }
 }
