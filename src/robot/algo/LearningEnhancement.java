52ea2fcd04e11a6b7a22a7db4394f4f34d64ccad
enzo_dechaene <enzo.dechaene76@gmail.com>
2020-02-17 15:53:33 +0100

    Modif Q learning class

--- src/robot/algo/LearningEnhancement.java
+++ src/robot/algo/LearningEnhancement.java
@@ -26,7 +26,7 @@ public class LearningEnhancement
     public Mode getTypeMove()
     {
         if (LearningEnhancement.random() < epsilon) {
-            epsilon -= MOVE_EPSILON;
+            //epsilon -= MOVE_EPSILON;
             return Mode.EXPLORATION;
         }
         else {
@@ -34,32 +34,6 @@ public class LearningEnhancement
             return Mode.OPERATION;
         }
     }
-
-    /*
-    void calculateQ( int trainCycles)
-    {
-        Random rand = new Random();
-        for ( int i = 0; i < trainCycles; i++) { // Train cycles
-            // Sélectionne un é tat initial alé atoire
-            int crtState = rand.nextInt(statesCount);
-
-            while (!isFinalState(crtState)) {
-
-                int [] actionsFromCurrentState = ←$ possibleActionsFromState(crtState);
-                // Ré cup ère une action alé atoire parmis les actions ←$ possibles
-                int index = rand.nextInt(actionsFromCurrentState.←$ length);
-                int nextState = actionsFromCurrentState[index];
-                // Mise à jour de la Q- table
-                double q = Q[crtState][nextState];
-                double maxQ = maxQ(nextState);
-                int r = R[crtState][nextState];
-                Q[crtState][nextState] = q + alpha * (r + gamma * maxQ←$ - q);
-                crtState = nextState;
-            }
-        }
-    }
-    
-     */
 
     /**
      * This function is use to have a random between two integer.
--- src/robot/classes/CentraliserRobot.java
+++ src/robot/classes/CentraliserRobot.java
@@ -233,7 +233,6 @@ public class CentraliserRobot extends Robot
         });
 
         return place;
-
     }
 
     /**
